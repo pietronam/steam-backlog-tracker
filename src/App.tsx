d618609214dd66582c1c2db8f0b97caddbbabf51
nam <pietronam.cavallanti@gmail.com>
2026-06-28 16:36:02 +0200

    implemented basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useSteamData } from "../context/SteamDataContext";
 
-export const Home = () => {
+export const Login = () => {
   const { state } = useSteamData();
 
   if (state.steamid !== 0) {
-    return <Navigate to="/home" replace />;
+    return <Navigate to="/" replace />;
   }
 
+  return <div>Login page</div>;
 }
